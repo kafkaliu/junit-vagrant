0ed4120365e8d10b6c10b50dedbb224ecf7f8fd2
kafka liu <kafka.liu@gmail.com>
2013-05-11 13:39:07 +0800

    Add another test runner for the testing in the vm.
    Through SSH (refer to the ssh_run.rb) to run single test by the method
    name in the vm. See also SingleJUnitTestRunner.

--- src/test/java/org/kafkaliu/test/vagrant/container/VagrantVirtualMachineTestRunnerTests.java
+++ src/test/java/org/kafkaliu/test/vagrant/container/VagrantVirtualMachineTestRunnerTests.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.text.MessageFormat;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kafkaliu.test.vagrant.server.annotations.VagrantConfigure;
@@ -14,29 +13,13 @@ import org.kafkaliu.test.vagrant.server.annotations.VagrantConfigure;
 public class VagrantVirtualMachineTestRunnerTests {
 
 	@Test
-	public void checkVmFlag() {
+	public void testTrue() {
 		System.out.println(MessageFormat.format("vagrant.isinvm={0}", System.getProperty("vagrant.isinvm")));
 		assertTrue("Should be in the vm.", Boolean.valueOf(System.getProperty("vagrant.isinvm")));
 	}
 	
-	@Ignore
-	@Test
-	public void checkClasspath() {
-		String[] classpaths = System.getProperty("java.class.path")
-				.split(System.getProperty("path.separator"));
-		
-		boolean found = false;
-		for (String cp : classpaths) {
-			if (cp.startsWith("/vagrant-junit")) {
-				found = true;
-				break;
-			}
-		}
-		assertTrue("Can not found the classpaths in the vm.", found);
-	}
-	
-//	@Test(expected=AssertionError.class)
-	public void test2() {
+	@Test(expected=AssertionError.class)
+	public void testException() {
 		assertTrue(false);
 	}
 
